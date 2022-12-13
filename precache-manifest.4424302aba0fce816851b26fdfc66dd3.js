19bbf173961c6b17ec0c24dce011e7cfda671949
Rajeshwar Singh <rsr45411@gmail.com>
2022-12-13 09:44:24 +0530

    Updates

---
+++ precache-manifest.4424302aba0fce816851b26fdfc66dd3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1ef01d537a726113291",
+    "url": "/profile/static/js/main.d1ef01d5.chunk.js"
+  },
+  {
+    "revision": "bf4a4a84b684452eb249",
+    "url": "/profile/static/js/1.bf4a4a84.chunk.js"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "0e786a047eea13a8e8ca6d16ea76ed00",
+    "url": "/profile/static/media/Mee.0e786a04.png"
+  },
+  {
+    "revision": "a9d77f42bb4b11bc40825d4624c66a57",
+    "url": "/profile/static/media/facebook-logo.a9d77f42.png"
+  },
+  {
+    "revision": "e2373858eb256fb25d9dd7ff712da8c6",
+    "url": "/profile/static/media/Linkedin-logo.e2373858.png"
+  },
+  {
+    "revision": "c3017fcba6cf7c4883f1fd75965e514f",
+    "url": "/profile/static/media/Whatsapp-logo.c3017fcb.png"
+  },
+  {
+    "revision": "3c5e7b266dd41dacced5daa2279555fe",
+    "url": "/profile/static/media/me.3c5e7b26.jpg"
+  },
+  {
+    "revision": "00a379b1baecb235e1e3c701595b43fe",
+    "url": "/profile/static/media/medal.00a379b1.png"
+  },
+  {
+    "revision": "46f4949cdaad00c7e9ac594b6390a342",
+    "url": "/profile/static/media/client.46f4949c.png"
+  },
+  {
+    "revision": "db8c7a5e630dde656006c917f23d2c2f",
+    "url": "/profile/static/media/projects.db8c7a5e.png"
+  },
+  {
+    "revision": "e8e94992858f0d6fd440dabe74937155",
+    "url": "/profile/static/media/contactPic.e8e94992.png"
+  },
+  {
+    "revision": "397fe2fdf698568635c2b80f9682d326",
+    "url": "/profile/index.html"
+  }
+];
